a95531ccaa84c434be8ad3a24078c90fd7fa6184
malkuthro <166633396+malkuthro@users.noreply.github.com>
2025-10-18 23:22:03 +0200

    Get info - deleted

--- web/ai_pipeline.js
+++ web/ai_pipeline.js
@@ -51,68 +51,6 @@ app.registerExtension({
     async nodeCreated(node) {
         if (node.comfyClass !== "EasyResize") return;
 
-        // Add multiline display widget
-        const displayWidget = ComfyWidgets["STRING"](node, "Details Preview", ["STRING", { multiline: true }], app).widget;
-        displayWidget.inputEl.readOnly = true;
-        displayWidget.inputEl.style.height = "100px";
-
-        // Function to get original details
-        const getOriginalDetails = (node, callback) => {
-            const imageInput = node.inputs?.find(i => i.name === "image");
-            if (!imageInput || !imageInput.link) {
-                displayWidget.value = "No image connected";
-                return;
-            }
-            const link = app.graph.links[imageInput.link];
-            const originNode = app.graph.getNodeById(link.origin_id);
-            if (!originNode || !originNode.imgs || originNode.imgs.length === 0) {
-                displayWidget.value = "No image loaded in source node";
-                return;
-            }
-            const frames = originNode.imgs.length;
-            const previewImg = originNode.imgs[0];
-            const img = new Image();
-            img.src = previewImg.src;
-            img.onload = () => {
-                const w = img.naturalWidth;
-                const h = img.naturalHeight;
-                function gcd(a, b) {
-                    return b === 0 ? a : gcd(b, a % b);
-                }
-                const d = gcd(w, h);
-                const ar_w = w / d;
-                const ar_h = h / d;
-                callback(w, h, frames, ar_w, ar_h);
-            };
-            img.onerror = () => {
-                displayWidget.value = "Error loading image";
-            };
-        };
-
-        // Button to show original details
-        node.addWidget("button", "Show Original Details", null, () => {
-            getOriginalDetails(node, (w, h, frames, ar_w, ar_h) => {
-                displayWidget.value = `Original Resolution: ${w}x${h}\nFrames: ${frames}\nAspect Ratio: ${ar_w}:${ar_h}`;
-                node.setDirtyCanvas(true, true);
-            });
-        }, { serialize: false });
-
-        // Callback for base_on
-        const baseOnWidget = node.widgets.find(w => w.name === "base_on");
-        const origBaseCallback = baseOnWidget.callback;
-        baseOnWidget.callback = (value) => {
-            if (origBaseCallback) origBaseCallback.call(node, value);
-            if (value === "Original") {
-                getOriginalDetails(node, (w, h, frames, ar_w, ar_h) => {
-                    const aspectWidget = node.widgets.find(w => w.name === "aspect_ratio");
-                    if (aspectWidget) {
-                        aspectWidget.value = `${ar_w}:${ar_h}`;
-                        node.setDirtyCanvas(true, true);
-                    }
-                });
-            }
-        };
-
         // Function to update widgets for a mode
         const updateWidgets = (modeName, colorName, value) => {
             const colorWidgetIndex = node.widgets.findIndex(w => w.name === colorName);
